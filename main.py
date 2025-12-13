f457f43bfae5a0cd6fe946fa858433812fc80374
Wajahat Haider <wajahathaider0@gmail.com>
2025-12-13 12:00:00 +0000

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from analysis import perform_serial_correlation_analysis, perform_full_factorial_and_metamodel
-
-if __name__ == "__main__":
-    print("Performing Serial Correlation Analysis:")
-    perform_serial_correlation_analysis()
-
-    print("\nPerforming Full Factorial Experiment and Metamodeling:")
-    perform_full_factorial_and_metamodel()
